996815ec1734fde6b884efba6b84dfaf4ec4b6a4
Le Mong Thuy <thuyle2000@gmail.com>
2023-08-02 09:03:51 +0700

    Upload Aug 2 , 2023

--- 5. DDD-DM/d03_Create Table.sql
+++ 5. DDD-DM/d03_Create Table.sql
@@ -88,3 +88,37 @@ create table tbStudent (
 )
 go
 
+select * from tbBatch
+select * from tbStudent
+go
+
+-- nhap du lieu cho bang sinh vien
+SET DATEFORMAT DMY
+INSERT tbStudent VALUES
+('sv01','Ha The Doanh',1, '18/03/2003','doanh@fpt.edu.vn','T1.2303.M0', null),
+('sv02','Le Tan Thanh',1, '14/01/2002','thanh@fpt.edu.vn','T1.2303.M0', 'sv01'),
+('sv03','Lee Min Hoo',1, '03/03/2003','minh@fpt.edu.vn','T1.2303.M0', 'sv01'),
+('sv04','Nguyen Nhat Truong',1, '21/05/1993','truong@fpt.edu.vn','T1.2303.M0', null),
+('sv05','Bui Tran Nguyen Hung',1, '05/11/2001','hung@fpt.edu.vn','T1.2303.M0', 'sv04'),
+('sv06','Nguyen Quang Linh',1, '20/10/1998','linh@fpt.edu.vn','T1.2303.M0', 'sv04'),
+('sv07','Vu Minh Tuan',1, '24/02/1997','tuan@fpt.edu.vn','T1.2303.M0', null),
+('sv08','Chu Quoc Huy',1, '10/06/2005','huy@fpt.edu.vn','T1.2303.M0', 'sv07'),
+('sv09','Bui My Nhung',0, '10/10/2006','nhung@fpt.edu.vn','T1.2303.M0', 'sv07'),
+('sv10','Ta Hai Luong',1, '20/08/1998','lookso@fpt.edu.vn','T1.2303.M0', null),
+('sv11','Nguyen Khanh Danh',1, '17/02/2001','danh@fpt.edu.vn','T1.2303.M0', 'sv10'),
+('sv12','Nguyen Tat Thien',1, '28/06/1998','thien@fpt.edu.vn','T1.2303.M0', 'sv10'),
+('sv13','Nguyen Van Phung',0, '14/01/2004','phung@fpt.edu.vn','T1.2303.M0', 'sv10'),
+('sv14','Doan Dien Khanh',0, '14/01/2003','khanh@fpt.edu.vn','T1.2303.M0', 'sv10'),
+('sv15','Nguyen Anh Khoa',1, '10/08/1993','khoa@fpt.edu.vn','T1.2303.M0', null),
+('sv16','Nguyen Ho Anh Ngoc ',0, '13/08/1998','Ngoc@fpt.edu.vn','T1.2303.M0', 'sv15'),
+('sv17','Trinh Duy Anh',1, '09/04/1988','anh@fpt.edu.vn','T1.2303.M0', 'sv15'),
+('sv18','Nguyen Lam Nguyen Vu',1, '06/12/2001','vu@fpt.edu.vn','T1.2303.M0', null),
+('sv19','Nguyen Minh Thien',1, '23/06/2005','thien-nguyen@fpt.edu.vn','T1.2303.M0', 'sv18'),
+('sv20','Do Nhat Hoang',1, '14/01/1999','hoang@fpt.edu.vn','T1.2303.M0', 'sv18'),
+('sv21','Nguyen Minh Trung',1, '26/05/2002','trung@fpt.edu.vn','T1.2104.E1', null),
+('sv22','Luu Quoc Hoang',1, '26/05/2002','hoang@fpt.edu.vn','T1.2104.E1', 'sv21'),
+('sv23','Dao Qui Phi',0, '26/04/2002','phi@fpt.edu.vn','T1.2104.E1', 'sv21')
+go
+
+select * from tbStudent
+go
